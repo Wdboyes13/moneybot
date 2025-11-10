ea313b94dac7d0b7678b3dc80b127c6827915a6b
Wdboyes13 <willdev2025@outlook.com>
2025-11-10 01:21:34 -0800

    Updated Mon 10 Nov 2025 01:21:34 PST

--- src/commands/bot_gamble.rs
+++ src/commands/bot_gamble.rs
@@ -7,17 +7,13 @@ cmdutil::sec_check!(gamble_check, 20);
 
 pub fn bot_gamble(gid: u64, uid: u64, amount: u32) -> String {
     let mut rng = rand::rng();
-    let mut avg: u64 = 0;
-    for _ in 1..=10 {
-        avg += rng.random_range(1..=10);
-    }
-    avg /= 10;
+    let rand: u64 = rng.random_range(1..=100);
 
     if let Ok(db) = botdb::MoneyDatabase::open(gid) {
         if let Err(err) = db.delete_money(uid, amount) {
             return err.to_string();
         }
-        if avg > 5 {
+        if rand > 55 {
             if let Ok(()) = db.add_money(uid, amount * 2) {
                 return format!("You won {}", amount * 2).to_string();
             } else {
